d0e99931bf3a8df79e5fc51629da38419f7e958b
Fonij80 <forooz.iraji@gmail.com>
2024-07-30 17:36:31 +0330

    add project profiling and BDD

---
+++ src/main/java/profiling/Utils.java
@@ -0,0 +1,14 @@
+package profiling;
+
+
+public class Utils {
+
+    public static void sleepUninterrruptable(double seconds) {
+        try {
+            Thread.sleep((long)(seconds * 1000));
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Sleep thread interrupted !");
+        }
+    }
+
+}
