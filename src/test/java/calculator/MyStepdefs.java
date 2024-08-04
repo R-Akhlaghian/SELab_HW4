d6e01f4f6fe6242793398ab7e5a54b3996e7e38e
Fonij80 <forooz.iraji@gmail.com>
2024-08-04 17:24:23 +0330

    add scenario square root

--- src/test/java/calculator/MyStepdefs.java
+++ src/test/java/calculator/MyStepdefs.java
@@ -5,6 +5,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 
 
 public class MyStepdefs {
@@ -34,6 +35,26 @@ public class MyStepdefs {
     @Then("^I expect the result (\\d+)$")
     public void iExpectTheResult(int arg0) {
         Assert.assertEquals(arg0, result);
+    }
+
+
+    @Given("^Two input values, a = (\\d+) and b = (\\d+)$")
+    public void twoInputValuesAAndB(int a, int b) {
+        this.value1 = a;
+        this.value2 = b;
+    }
+
+    @When("^I calculate the square root of a/b$")
+    public void iCalculateTheSquareRootOfAB() {
+        if (this.value2 == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        this.result = (int) Math.sqrt((double) this.value1 / this.value2);
+    }
+
+    @Then("^I expect the result to be ([\\d.]+)$")
+    public void iExpectTheResultToBe(double expectedResult) {
+        assertEquals(expectedResult, this.result, 0.001);
     }
 
 }
--- src/test/resources/features/calculator.feature
+++ src/test/resources/features/calculator.feature
@@ -5,3 +5,9 @@ Feature: Calculator
         Given Two input values, 1 and 2
         When I add the two values
         Then I expect the result 3
+
+
+    Scenario: calculate square root of a/b
+        Given Two input values, a = 4 and b = 1
+        When I calculate the square root of a/b
+        Then I expect the result to be 2
